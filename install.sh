0c141ff3e15b050325ff87c692c635b920cce076
Lerry <lvdachao@gmail.com>
2014-01-24 15:39:37 +0800

    f

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ sudo chmod +x /usr/bin/rs
 sudo cp ack /usr/bin/ack
 sudo chmod +x /usr/bin/ack
 
+sudo cp dirreplace /usr/bin/dirreplace
+sudo chmod +x /usr/bin/dirreplace
+
 sh vim.sh
 sh zsh.sh
 
